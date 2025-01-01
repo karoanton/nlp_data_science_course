be6a818e103d2e73dc70e23c4e04d166be3388ed
karoanton <152211728+karoanton@users.noreply.github.com>
2025-01-01 16:13:47 -0500

    updates to insurance portfolio project

---
+++ U.S. Medical Insurance Portfolio Project.py
@@ -0,0 +1,21 @@
+"""
+Project goals:
+Region: which area has the most patients?
+Age & Smoking: what is the average age for smokers vs. non-smokers?
+Children and Cost: What is the average cost for each number of children a person may or may not have?
+"""
+
+
+# import necessary packages
+#import pandas as pd
+import csv
+
+
+# Save dataset via Python variable
+age = []
+sex = []
+bmi = []
+children = []
+smoker = []
+region = []
+charges = []

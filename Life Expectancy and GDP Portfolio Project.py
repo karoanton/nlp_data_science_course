ea776af649615eedb4c9195e4a059ae8e0197f3b
karoanton <152211728+karoanton@users.noreply.github.com>
2025-02-26 15:17:40 -0500

    life expectancy and gdp portfolio project

---
+++ Life Expectancy and GDP Portfolio Project.py
@@ -0,0 +1,9 @@
+"""
+For this project, you will analyze data on GDP and life expectancy from the World Health Organization and the World Bank
+to try and identify the relationship between the GDP and life expectancy of six countries.
+
+During this project, you will analyze, prepare, and plot data in order to answer questions in a meaningful way.
+
+After you perform your analysis, you’ll be creating a blog post to share your findings on the World Health Organization
+website.
+"""
